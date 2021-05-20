e734465998c1c3bb1b9619807e41b7578a01d729
rsousaj <rafasousa@gmail.com>
2021-05-20 14:35:26 -0300

    garbage collector...

--- src/main/java/br/com/zup/orangetalents/casadocodigo/CasaDoCodigoApplication.java
+++ src/main/java/br/com/zup/orangetalents/casadocodigo/CasaDoCodigoApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @SpringBootApplication
 public class CasaDoCodigoApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(CasaDoCodigoApplication.class, args);
-		
-		ObjectMapper mapper = context.getBean(ObjectMapper.class);
+		SpringApplication.run(CasaDoCodigoApplication.class, args);
 	}
 
 }
